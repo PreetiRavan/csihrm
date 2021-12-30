6be81b343793b5d6346e6072c96da11c7968a735
unknown <ravanpreeti1998@gmail.com>
2021-12-30 12:45:18 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # csihrm
+
+# Hrm variables
---
+++ csihrmpapp123/src/main/java/com/csi/Csihrmpapp123Application.java
@@ -0,0 +1,13 @@
+package com.csi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Csihrmpapp123Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Csihrmpapp123Application.class, args);
+	}
+
+}
